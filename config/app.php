7284d03de81da470452338606faf6546d99e2ea9
Thiago Mendes <thiagotmendes@outlook.com>
2017-01-24 09:58:21 -0200

    iniciando desenvolvimento

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','hTlQEXcGUgW42VsSzn2mW+q'),
 
     'cipher' => 'AES-256-CBC',
 
